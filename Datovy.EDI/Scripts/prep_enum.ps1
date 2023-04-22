083f6114085ead25f89f4206703001f8b96263b0
esobrino <esobrino@openk.com>
2023-04-22 07:48:20 -0400

    added prep_enum script

---
+++ Datovy.EDI/Scripts/prep_enum.ps1
@@ -0,0 +1,13 @@
+﻿param([String]$filepath);
+
+# read all csv lines into an array
+$data = Import-csv -Path $filepath -Header "enum", "desc" -Delimiter "|"
+foreach($i in $data)
+{
+   write("            <xs:enumeration value=`""+ $i.enum + "`">");
+   write("               <xs:annotation>");
+   write("                  <xs:appinfo>" + $i.desc + "</xs:appinfo>");
+   write("               </xs:annotation>");
+   write("            </xs:enumeration>");
+}
+   
