03ca5dfc7d2d9c3b23328a02f82a7ca097f14f36
esobrino <esobrino@openk.com>
2023-05-27 17:01:41 -0400

    Added entity-link to EDI definition...

--- Datovy.EDI/Scripts/prep_enum.ps1
+++ Datovy.EDI/Scripts/prep_enum.ps1
@@ -4,6 +4,11 @@
 $data = Import-csv -Path $filepath -Header "enum", "desc" -Delimiter "|"
 foreach($i in $data)
 {
+   if ($i.desc == "" or $i.desc == null)
+   {
+      echo "ups"
+   }
+
    write("            <xs:enumeration value=`""+ $i.enum + "`">");
    write("               <xs:annotation>");
    write("                  <xs:appinfo>" + $i.desc + "</xs:appinfo>");
